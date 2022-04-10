757a4ce6d1634d0eae327bb8f3ad254836b3d826
Ayako-Yokoe <niluvsuall@gmail.com>
2022-04-09 17:17:11 -0700

    changed the structure

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ app.use('/api/carts', cartRoute)
 app.use('/api/orders', orderRoute)
 app.use('/api/checkout', stripeRoute)
 app.use('/api/favorite', favoriteRoute)
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 
 app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname + '/../client/build/index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
 
 
