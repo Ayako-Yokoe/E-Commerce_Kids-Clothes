688a928ae997b5f687b838f93d48d1a715fdfbf4
Ayako-Yokoe <niluvsuall@gmail.com>
2022-04-10 19:54:48 -0700

    make it better

--- index.js
+++ index.js
@@ -28,12 +28,21 @@ app.use('/api/carts', cartRoute)
 app.use('/api/orders', orderRoute)
 app.use('/api/checkout', stripeRoute)
 app.use('/api/favorite', favoriteRoute)
-app.use(express.static(path.join(__dirname, 'client', 'build')))
+// app.use(express.static('client/build'))
 
 
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-})
+
+// app.get('*', (req,res) => {
+//     res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+// })
+
+if (process.env.NODE_ENV === 'production') {           
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 
 app.listen(process.env.PORT || 8000, () => {
