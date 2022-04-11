a9dc3657c99bbeca13c8cee0b08915bf4d19d288
Ayako-Yokoe <niluvsuall@gmail.com>
2022-04-11 00:14:43 -0700

    make it better

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,19 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
+// const { createProxyMiddleware } = require('http-proxy-middleware')
 
-module.exports = function(app){
+// module.exports = function(app){
+//     app.use(
+//         ['/api', '/api/*'],
+//         createProxyMiddleware({ target: "http://localhost:8000" })
+//     )
+// }
+
+const {createProxyMiddleware} = require('http-proxy-middleware');
+
+module.exports = (app) => {
     app.use(
-        ['/api', '/api/*'],
-        createProxyMiddleware({ target: "http://localhost:8000" })
-    )
-}
+        createProxyMiddleware('/api/*', {
+            target: 'http://localhost:8000/',
+            secure: false,
+        }),
+    );
+};
